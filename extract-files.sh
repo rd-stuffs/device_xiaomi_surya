b8e13a25c9c109a6f8308368dd347434b280fc53
BladeRunner-A2C <john.smith@unused.email>
2024-05-05 16:10:21 +0600

    surya: Patch camera provider to load the new symbols
    Change-Id: Ide88f50ca68dbd0791a22326153f22390187f206
    Co-authored-by: Giovanni Ricca <giovanniricca@proton.me>
    Signed-off-by: BladeRunner-A2C <john.smith@unused.email>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,10 @@ function blob_fixup() {
             [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
+        vendor/lib/android.hardware.camera.provider@2.4-legacy.so | vendor/lib64/android.hardware.camera.provider@2.4-legacy.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcamera_provider_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_provider_shim.so" "${2}"
+            ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
             [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
